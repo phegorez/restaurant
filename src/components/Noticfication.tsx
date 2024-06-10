6ad7ba7cc2eb3263710a98c97421518a97604511
phegorez <metha.nompd@gmail.com>
2024-06-10 21:48:31 +0700

    front-end already

---
+++ src/components/Noticfication.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Noticfication = () => {
+    return (
+        <div className='cursor-pointer text-center text-sm h-12 bg-red-500 text-white px-4 flex items-center justify-center md:text-base'>Free delivery for all orders over $50. Order your food now!</div>
+    )
+}
+
+export default Noticfication
